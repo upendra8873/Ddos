17021e72b348150c509f5944b22930794f3af96c
upendra8873 <59110813+upendra8873@users.noreply.github.com>
2024-10-24 08:46:30 +0530

    Update sam.py

--- sam.py
+++ sam.py
@@ -170,7 +170,7 @@ async def bgmi(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     port = context.args[1]
     duration = context.args[2]
 
-    command = ['./soul', target_ip, port, duration, str(DEFAULT_THREADS)]
+    command = ['./soulcracks', target_ip, port, duration, str(DEFAULT_THREADS)]
 
     process = subprocess.Popen(command)
     
